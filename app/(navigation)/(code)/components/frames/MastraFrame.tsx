9851f4b79d3329c1c64ec9cf1a7a092e2995e4df
Paul Scanlon <pauliescanlon@gmail.com>
2026-04-28 09:05:36 +0100

    feat: add new mastra theme - second pass

--- app/(navigation)/(code)/components/frames/MastraFrame.tsx
+++ app/(navigation)/(code)/components/frames/MastraFrame.tsx
@@ -1,11 +1,9 @@
 import classNames from "classnames";
 import { useAtom, useAtomValue } from "jotai";
 
-import { fileNameAtom, showBackgroundAtom } from "../../store";
-import { flashShownAtom } from "../../store/flash";
+import { showBackgroundAtom } from "../../store";
 import { paddingAtom } from "../../store/padding";
 import { themeDarkModeAtom } from "../../store/themes";
-import FileIcon from "../../assets/mastra-file.svg";
 
 import Editor from "../Editor";
 import sharedStyles from "./DefaultFrame.module.css";
@@ -15,10 +13,6 @@ const MastraFrame = () => {
   const darkMode = useAtomValue(themeDarkModeAtom);
   const [padding] = useAtom(paddingAtom);
   const [showBackground] = useAtom(showBackgroundAtom);
-  const [fileName, setFileName] = useAtom(fileNameAtom);
-  const flashShown = useAtomValue(flashShownAtom);
-
-  const showTab = fileName.length > 0 || !flashShown;
 
   return (
     <div
@@ -41,32 +35,10 @@ const MastraFrame = () => {
             <svg className={styles.cornerCircleTL} aria-hidden viewBox="0 0 50 50">
               <circle cx="25" cy="25" r="24" strokeWidth="1" strokeDasharray="6 6" />
             </svg>
-            <svg className={styles.cornerCircleTR} aria-hidden viewBox="0 0 50 50">
-              <circle cx="25" cy="25" r="24" strokeWidth="1" strokeDasharray="6 6" />
-            </svg>
-            <svg className={styles.cornerCircleBL} aria-hidden viewBox="0 0 50 50">
-              <circle cx="25" cy="25" r="24" strokeWidth="1" strokeDasharray="6 6" />
-            </svg>
             <svg className={styles.cornerCircleBR} aria-hidden viewBox="0 0 50 50">
               <circle cx="25" cy="25" r="24" strokeWidth="1" strokeDasharray="6 6" />
             </svg>
           </>
-        )}
-        {showTab && (
-          <div className={styles.tile} data-ignore-in-export={fileName.length === 0 ? "" : undefined}>
-            <FileIcon className={styles.fileIcon} aria-hidden />
-            <div className={classNames(sharedStyles.fileName, styles.fileName)} data-value={fileName}>
-              <input
-                type="text"
-                value={fileName}
-                onChange={(event) => setFileName(event.target.value)}
-                spellCheck={false}
-                tabIndex={-1}
-                size={1}
-              />
-              {fileName.length === 0 ? <span>Untitled-1</span> : null}
-            </div>
-          </div>
         )}
         <div className={styles.window}>
           <Editor />
